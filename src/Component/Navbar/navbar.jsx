31bfa4668edfff9b4fd0627c4a371b5a1d7add89
Wilson <145624344+Wilson1684@users.noreply.github.com>
2025-03-19 04:29:35 +0800

    Update navbar.jsx

--- src/Component/Navbar/navbar.jsx
+++ src/Component/Navbar/navbar.jsx
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet';
 import { FaBars, FaTimes } from "react-icons/fa";
 import { logoutUser, fetchUserData, fetchGoogleUserData } from '../../../../Api/api';
 import './navbar.css';
-import DefaultAvatar from '../../../../Frontend/src/public/avatar.png';
+import DefaultAvatar from '../../../src/public/avatar.png';
 import eventBus from '../EventBus/EventBus';
 
 function Navbar() {
