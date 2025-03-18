fd8b702034f76652e1615ae6756d5e32158564c8
Wilson <145624344+Wilson1684@users.noreply.github.com>
2025-03-18 13:34:37 +0800

    Update contact_us.jsx

--- src/Component/Contact Us/contact_us.jsx
+++ src/Component/Contact Us/contact_us.jsx
@@ -59,16 +59,12 @@ const Contact = () => {
         <div className='container_contact_us'>
           <div className='google_map'>
             <iframe 
-             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.9586177612214!2d110.31007237509338!3d1.749442560160908!2m3
-             !1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31faff9851a3becb%3A0xf308ff203e894002!2sDamai%20Beach!5e0!3m2!1sen!2smy!4v
-             1731252464570!5m2!1sen!2smy"
-             width="600" 
-             height="450"
-             style={{border: 0}}
-             allowfullscreen=""
-             loading="lazy"
-             referrerpolicy="no-referrer-when-downgrade">
-             </iframe>
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.9586177612214!2d110.31007237509338!3d1.749442560160908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31faff9851a3becb%3A0xf308ff203e894002!2sDamai%20Beach!5e0!3m2!1sen!2smy!4v1731252464570!5m2!1sen!2smy"
+              style={{border: 0, width: '100%', height: '450px'}}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade">
+            </iframe>
           </div>
 
           <div className='contactForm'>
@@ -110,7 +106,7 @@ const Contact = () => {
               <button type='submit' className='btn_submit'>
                 <span>Send</span>
               </button>
-              <p>{status}</p> {/* Display status message */}
+              <p>{status}</p>
             </form>
           </div>
         </div>
