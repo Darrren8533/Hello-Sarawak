a931ba7a2b0a2476119c8ac275ec120500c6e116
Wilson <145624344+Wilson1684@users.noreply.github.com>
2025-03-20 11:21:43 +0800

    Update navbar.jsx

--- src/Component/Navbar/navbar.jsx
+++ src/Component/Navbar/navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
         const checkLoginStatus = async () => {
             if (userID) {
                 try {
-                    const response = await fetch(`http://localhost:5000/checkStatus?userID=${userID}`);
+                    const response = await fetch(`https://cams-backend.vercel.app/checkStatus?userID=${userID}`);
                     const data = await response.json();
 
                     if (data.uStatus === 'login') {
