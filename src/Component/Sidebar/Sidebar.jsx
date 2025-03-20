65e433d6bf83ff84f8899e4ad9f740d0295cbfe3
DarrenLau <102768533@students.swinburne.edu.my>
2025-03-20 14:43:12 +0800

    Update Sidebar.jsx

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = ({ title, links, isCollapsed, toggleSidebar }) => {
 
       if (userID) {
         try {
-          const response = await fetch(`http://localhost:5000/checkStatus?userID=${userID}`);
+          const response = await fetch(`https://cams-backend.vercel.app/checkStatus?userID=${userID}`);
           const data = await response.json();
 
           if (data.uStatus === 'login') {
