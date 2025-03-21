1330481464671ccb034a4af1a49c85d99c904ec9
DarrenLau <102768533@students.swinburne.edu.my>
2025-03-22 00:10:08 +0800

    Update navbar.jsx

--- src/Component/Navbar/navbar.jsx
+++ src/Component/Navbar/navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
     useEffect(() => {
         const checkLoginStatus = async () => {
             if (userID) {
-                
+                console.log(userID);
                 try {
                     const response = await checkstatus(userID);
                     const data = await response.json();
