3604fe8ce152f4efb951891ca15fd3c904b0bfa3
Jun Yu <118051814+Gamer1022@users.noreply.github.com>
2025-03-24 11:29:01 +0800

    Update Login.jsx

--- src/Pages/customer/Login/Login.jsx
+++ src/Pages/customer/Login/Login.jsx
@@ -100,7 +100,7 @@ const Login = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('http://localhost:5000/forgot-password', {
+      const response = await fetch('https://cams-backend.vercel.app/forgot-password', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
