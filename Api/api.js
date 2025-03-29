d9fd09c467ffa23eb631713d84934b5b2633c4be
ChongKaiBo <128211069+ChongKaiBo@users.noreply.github.com>
2025-03-29 14:28:13 +0800

    Update api.js

--- Api/api.js
+++ Api/api.js
@@ -715,6 +715,17 @@ export const fetchFinance = async () => {
   }
 };
 
+export const fetchOccupancyRate = async () => {
+  try {
+      const response = await fetch(`${API_URL}/users/occupancy_rate`);
+      const data = await response.json();
+      return data; 
+  } catch (error) {
+      console.error('API error:', error);
+      throw error;
+  }
+};
+
 // Get Properties Of Administrator For "Suggest"
 export const getOperatorProperties = async (userid) => {
   try {
