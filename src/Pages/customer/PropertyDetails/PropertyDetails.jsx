4e1c46d031e504405812d99b944d3d9adef03ba0
Jun Yu <118051814+Gamer1022@users.noreply.github.com>
2025-04-04 10:46:13 +0800

    Update PropertyDetails.jsx

--- src/Pages/customer/PropertyDetails/PropertyDetails.jsx
+++ src/Pages/customer/PropertyDetails/PropertyDetails.jsx
@@ -226,7 +226,7 @@ const PropertyDetails = () => {
         setShowBookingForm(false);
         setTimeout(() => {
         navigate('/cart');
-      }, 1000);
+      }, 5000);
 
     } catch (error) {
       console.error('Reservation error:', error);
