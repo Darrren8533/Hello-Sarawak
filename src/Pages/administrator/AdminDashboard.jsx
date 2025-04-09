77323647da34b0a5c394c03fea94799b2db88b00
ChongKaiBo <128211069+ChongKaiBo@users.noreply.github.com>
2025-04-09 10:13:03 +0800

    Update AdminDashboard.jsx

--- src/Pages/administrator/AdminDashboard.jsx
+++ src/Pages/administrator/AdminDashboard.jsx
@@ -18,6 +18,7 @@ import { FaUserTie, FaHotel } from 'react-icons/fa';
 import { FaBuildingUser } from "react-icons/fa6";
 import { VscGraphLine } from "react-icons/vsc";
 import { CgProfile } from "react-icons/cg";
+import { MdHistory } from "react-icons/md";
 import '../../Component/MainContent/MainContent.css';
 
   const AdminDashboard = () => {
@@ -64,7 +65,7 @@ import '../../Component/MainContent/MainContent.css';
     { path: '/administrator_dashboard/property-listing', label: 'PropertyListing', icon: <FaHotel />},
     { path: '/administrator_dashboard/reservations', label: 'Reservation', icon: <FiCalendar /> },
     { path: '/administrator_dashboard/booknpay-log', label: 'BooknPayLog', icon: <GoLog /> },
-    { path: '/administrator_dashboard/audit-trails', label: 'AuditTrails', icon: <GoLog /> },
+    { path: '/administrator_dashboard/audit-trails', label: 'AuditTrails', icon: <MdHistory /> },
     { path: '/administrator_dashboard/finance', label: 'Finance', icon: <VscGraphLine /> },
     { path: '/administrator_dashboard/profile', label: 'Profile', icon: <CgProfile /> },
   ];
