6cb71cc28e0afcb1d876e2943cd3b5ff1383b087
Wilson <145624344+Wilson1684@users.noreply.github.com>
2025-04-30 16:03:09 +0800

    Update Home.jsx

--- src/Pages/customer/Home/Home.jsx
+++ src/Pages/customer/Home/Home.jsx
@@ -7,21 +7,24 @@ import Contact from '../../../Component/Contact Us/contact_us';
 import Back_To_Top_Button from '../../../Component/Back_To_Top_Button/Back_To_Top_Button';
 import TawkMessenger from '../../../Component/TawkMessenger/TawkMessenger';
 import { AuthProvider } from '../../../Component/AuthContext/AuthContext';
+import './Home.css';
 
-const Customer_HomePage = () => {
+const Home = () => {
   return (
     <div>
-      <AuthProvider>
-      <Navbar />
-      <Slider />
-      <Destination />
-      <Contact />
-      <Back_To_Top_Button />
-      <Footer />
-      <TawkMessenger />
-      </AuthProvider>
+      <div className="Home_Container_Main">
+        <AuthProvider>
+        <Navbar />
+        <Slider />
+        <Destination />
+        <Contact />
+        <Back_To_Top_Button />
+        <Footer />
+        <TawkMessenger />
+        </AuthProvider>
+      </div>
     </div>
   );
 };
 
-export default Customer_HomePage;
+export default Home;
