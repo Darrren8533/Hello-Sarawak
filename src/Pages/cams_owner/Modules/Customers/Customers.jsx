0d92a93378d9d541134702741ec512e801f536b9
Jun Yu <118051814+Gamer1022@users.noreply.github.com>
2025-06-19 20:49:37 +0800

    Update Customers.jsx

--- src/Pages/cams_owner/Modules/Customers/Customers.jsx
+++ src/Pages/cams_owner/Modules/Customers/Customers.jsx
@@ -114,11 +114,19 @@ const Customers = () => {
     };
 
     const columns = [
+        { header: 'UID', accessor: 'userid' },
         {
-            header: 'Customer',
+            header: 'Username',
             accessor: 'customer',
             render: (customer) => (
                <UserActivityCell user={customer} />
+            ),
+        },
+        {
+            header: 'Name',
+            accessor: 'name',
+            render: (customer) => (
+                `${customer.ufirstname.trim()} ${customer.ulastname.trim()}`
             ),
         },
         { header: 'Email', accessor: 'uemail' },
